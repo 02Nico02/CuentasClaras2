412f8d8ca320d64d8694c4cef2791d85254d970f
02Nico02 <ika.22.94@gmail.com>
2023-12-21 15:20:25 -0300

    Notificaciones

--- frontend/src/app/shared/nav/nav.component.ts
+++ frontend/src/app/shared/nav/nav.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { LoginService } from '../../services/auth/login.service';
+import { Router } from '@angular/router';
+import { NotificationComponent } from '../notification/notification.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NotificationComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   @Input() pagActual: string | undefined;
+
+  saldoUsuario: number = 0.0;
+  isDropdownOpen: boolean = false;
+
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  formatBalance(): string {
+    if (this.saldoUsuario > 0) {
+      return `+$${this.saldoUsuario.toFixed(2)}`;
+    } else if (this.saldoUsuario < 0) {
+      return `-$${Math.abs(this.saldoUsuario).toFixed(2)}`;
+    } else {
+      return '$0';
+    }
+  }
+
+  logout() {
+    this.loginService.logout().subscribe(
+      () => {
+        console.log("cerrada");
+      },
+      error => {
+        console.error("Error al cerrar sesión:", error);
+      }
+    );
+    this.router.navigate(['/login'])
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
