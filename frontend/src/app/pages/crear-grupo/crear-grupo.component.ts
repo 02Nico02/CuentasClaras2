5ff10487fddafda2e06fdc51874a7472cca7fb88
YanasuLucaroni <81329768+YanasuLucaroni@users.noreply.github.com>
2023-12-22 11:17:41 -0300

    Ya se crean los grupos correctamente

--- frontend/src/app/pages/crear-grupo/crear-grupo.component.ts
+++ frontend/src/app/pages/crear-grupo/crear-grupo.component.ts
@@ -19,13 +19,13 @@ export class CrearGrupoComponent implements OnInit {
   grupoForm: FormGroup;
 
   categorias: any[] = [
-    { id: 1, nombre: 'Familia', icon: 'icon' },
-    { id: 2, nombre: 'Viaje', icon: 'icon' },
-    { id: 3, nombre: 'Salida', icon: 'icon' },
-    { id: 4, nombre: 'Trabajo', icon: 'icon' },
-    { id: 5, nombre: 'Amigos', icon: 'icon' },
-    { id: 6, nombre: 'Estudio', icon: 'icon' },
-    { id: 7, nombre: 'Deportes', icon: 'icon' },
+    { id: 1, nombre: 'Salida', icon: 'icon' },
+    { id: 2, nombre: 'Trabajo', icon: 'icon' },
+    { id: 3, nombre: 'Familia', icon: 'icon' },
+    { id: 4, nombre: 'Amigos', icon: 'icon' },
+    { id: 5, nombre: 'Estudio', icon: 'icon' },
+    { id: 6, nombre: 'Deportes', icon: 'icon' },
+    { id: 7, nombre: 'Viaje', icon: 'icon' },
   ];
 
   constructor(private formBuilder: FormBuilder, private groupService: GroupService, private titleService: Title, private router: Router) {
@@ -41,18 +41,26 @@ export class CrearGrupoComponent implements OnInit {
   crearGrupo(): void {
     if (this.grupoForm.valid) {
       const grupoData = this.grupoForm.value;
-      // this.groupService.crearGrupo(grupoData).subscribe(
-      //   response => {
-      //     console.log('Grupo creado con éxito:', response);
-      //   },
-      //   error => {
-      //     console.error('Error al crear el grupo:', error);
-      //   }
-      // );
+      let aux={
+        nombre:grupoData.nombre,
+        categoria:{
+          id:grupoData.categoria
+        }
+      }
+      let grupoId: number;
+      this.groupService.crearGrupo(aux).subscribe({
+        next:(response) => {
+          console.log('Grupo creado con éxito:', response);
+          grupoId = response.id
+        },
+        error:(error) => {
+          console.error('Error al crear el grupo:', error);
+        },
+        complete:()=>{
+          this.router.navigate(['/grupo', grupoId, 'detalle']);
+        }
+      });
 
-      // Por el momento un id base
-      const grupoId = 1
-      this.router.navigate(['/grupo', grupoId, 'detalle']);
     }
   }
 
--- frontend/src/app/services/group/group.service.ts
+++ frontend/src/app/services/group/group.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { GrupoDTO } from './grupo.dto';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +24,13 @@ export class GroupService {
     }
     console.log(`${environment.urlApi}pay/new`)
     return this.http.post<any>(`${environment.urlApi}pay/new`, aux)
+  }
+
+  crearGrupo(grupo: any): Observable<any>{
+    console.log(grupo)
+    return this.http.post<any>(environment.urlApi + "group/newGroup", grupo)
+      
+    
+    
   }
 }
